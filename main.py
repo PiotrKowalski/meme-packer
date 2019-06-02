44ca25db541868060b39d9bce1f726610184956a
Piotr Kowalski <piotreku9@o2.pl>
2019-06-02 14:21:50 +0200

    Little indentation fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 import logging
 
+
 def calculate(usb_size, memes):
     """
     Returns the most valuable memes from given data and usb size
@@ -24,9 +25,7 @@ def calculate(usb_size, memes):
         filtered_memes.append([list(combination) for combination in combinations(memes, len(memes)-index) if
                               sum([meme[1] for meme in list(combination)]) <= usb_size])
     sorted_memes = []
-
     for memes in filter(lambda list_of_memes: list_of_memes, filtered_memes):
         [sorted_memes.append(meme) for meme in memes]
     sorted_memes.sort(key=lambda item: sum([i[2] for i in item]), reverse=True)
     return sum([i[2] for i in sorted_memes[0]]), {i[0] for i in sorted_memes[0]}
-
