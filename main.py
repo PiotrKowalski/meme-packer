22509083d9ac336372e1512597d2be0768962c1f
Piotr Kowalski <piotreku9@o2.pl>
2019-06-02 14:20:16 +0200

    FIxing for code for one edge test case

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 import logging
 
-
 def calculate(usb_size, memes):
     """
     Returns the most valuable memes from given data and usb size
@@ -22,12 +21,12 @@ def calculate(usb_size, memes):
     filtered_memes = []
 
     for index, meme in enumerate(memes):
-        filtered_memes.append([list(combination) for combination in combinations(memes, index) if
-                              sum([meme[1] for meme in list(combination)]) < usb_size])
+        filtered_memes.append([list(combination) for combination in combinations(memes, len(memes)-index) if
+                              sum([meme[1] for meme in list(combination)]) <= usb_size])
     sorted_memes = []
 
-    for memes in filter(lambda list_of_memes: list_of_memes is not None, filtered_memes):
+    for memes in filter(lambda list_of_memes: list_of_memes, filtered_memes):
         [sorted_memes.append(meme) for meme in memes]
     sorted_memes.sort(key=lambda item: sum([i[2] for i in item]), reverse=True)
-
     return sum([i[2] for i in sorted_memes[0]]), {i[0] for i in sorted_memes[0]}
+
--- test_main.py
+++ test_main.py
@@ -10,7 +10,8 @@ from main import calculate
         (1, [('rollsafe.jpg', 205, 6), ('yodeling_kid.avi', 605, 12),('yodeling_kid.avi', 605, 12), ('sad_pepe_compilation.gif', 410, 10), ('rollsafe.jpg', 205, 6),('yodeling_kid.avi', 605, 12)], (22, {'sad_pepe_compilation.gif', 'yodeling_kid.avi'})),
         (1, [('dolan.png', 126, 5), ('expanding_brain.jpeg', 421, 10)], (15, {'dolan.png', 'expanding_brain.jpeg'})),
         (1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10),('yodeling_kid.avi', 126, 11),('I_got_an_arrow.jpg', 584, 20),('sad_adventurer.gif', 320, 25),('be_like_bill.avi', 175, 16),('But_Thats_None_of_My_Business.jpg', 105, 10),
-             ('grumpy_cat.gif', 210, 19),('old_spice_guy.avi', 105, 14),('rickroll.jpg', 265, 9),('doge_the_dog.gif', 320, 15),('leekspin.avi', 635, 11),('doggo.avi', 1000, 100)], (100, {'doggo.avi'}))
+             ('grumpy_cat.gif', 210, 19),('old_spice_guy.avi', 105, 14),('rickroll.jpg', 265, 9),('doge_the_dog.gif', 320, 15),('leekspin.avi', 635, 11),('doggo.avi', 1000, 100)], (100, {'doggo.avi'})),
+        (-1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10)], None)
     ]
 )
 def test_calculate(usb_size, memes, expected):
