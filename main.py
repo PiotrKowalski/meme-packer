e623f5d86364fe05358c605fdb8ddf7dac2af78c
Piotr Kowalski <piotreku9@o2.pl>
2019-06-03 23:45:55 +0200

    Better error handling

--- main.py
+++ main.py
@@ -12,20 +12,19 @@ def calculate(usb_size, memes):
     """
     if not memes:
         logging.error('No memes were given')
-        return None
     elif usb_size <= 0:
         logging.error('Wrong size of USB stick')
-        return None
-    usb_size *= 1024
-    memes = list(set(memes))
-    memes.sort(key=lambda item: item[2]/item[1], reverse=True)
-    filtered_memes = []
+    else:
+        usb_size *= 1024
+        memes = list(set(memes))
+        memes.sort(key=lambda item: item[2]/item[1], reverse=True)
+        filtered_memes = []
 
-    for index, meme in enumerate(memes):
-        filtered_memes.append([list(combination) for combination in combinations(memes, len(memes)-index) if
-                              sum([meme[1] for meme in list(combination)]) <= usb_size])
-    sorted_memes = []
-    for memes in filter(lambda list_of_memes: list_of_memes, filtered_memes):
-        [sorted_memes.append(meme) for meme in memes]
-    sorted_memes.sort(key=lambda item: sum([i[2] for i in item]), reverse=True)
-    return sum([i[2] for i in sorted_memes[0]]), {i[0] for i in sorted_memes[0]}
+        for index, meme in enumerate(memes):
+            filtered_memes.append([list(combination) for combination in combinations(memes, len(memes)-index) if
+                                  sum([meme[1] for meme in list(combination)]) <= usb_size])
+        sorted_memes = []
+        for memes in filter(lambda list_of_memes: list_of_memes, filtered_memes):
+            [sorted_memes.append(meme) for meme in memes]
+        sorted_memes.sort(key=lambda item: sum([i[2] for i in item]), reverse=True)
+        return sum([i[2] for i in sorted_memes[0]]), {i[0] for i in sorted_memes[0]}
--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import pytest
+import logging
 
 from main import calculate
 
@@ -11,7 +12,8 @@ from main import calculate
         (1, [('dolan.png', 126, 5), ('expanding_brain.jpeg', 421, 10)], (15, {'dolan.png', 'expanding_brain.jpeg'})),
         (1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10),('yodeling_kid.avi', 126, 11),('I_got_an_arrow.jpg', 584, 20),('sad_adventurer.gif', 320, 25),('be_like_bill.avi', 175, 16),('But_Thats_None_of_My_Business.jpg', 105, 10),
              ('grumpy_cat.gif', 210, 19),('old_spice_guy.avi', 105, 14),('rickroll.jpg', 265, 9),('doge_the_dog.gif', 320, 15),('leekspin.avi', 635, 11),('doggo.avi', 1000, 100)], (100, {'doggo.avi'})),
-        (-1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10)], None)
+        (-1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10)], logging.error('Wrong size of USB stick')),
+        (-1, [], logging.error('No memes were given'))
     ]
 )
 def test_calculate(usb_size, memes, expected):
